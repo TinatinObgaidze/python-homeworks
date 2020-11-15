28dbd4811317c929d583ac0884c433ac96b7af3c
tinatin1obgaidze <74489333+tinatin1obgaidze@users.noreply.github.com>
2020-11-16 00:05:32 +0400

    Add files via upload
    sacdeli homework

---
+++ main.py
@@ -0,0 +1,24 @@
+n = 0
+while n <= 10:
+    n += 1
+    print(n)
+    if n == 3:
+        break
+n = 5
+while n < 20:
+    n += 2
+    print(n)
+    if n == 17:
+        break
+n = 15
+while n < 25:
+    n += 2
+    print(n)
+    if n == 23:
+        break
+n = 0
+while n < 10:
+    n += 2
+    print(n)
+    if n == 14:
+        break
