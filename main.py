8c968d3a615f880834482f0a55e5d82a6420c861
tinatin1obgaidze <74489333+tinatin1obgaidze@users.noreply.github.com>
2020-11-27 15:45:04 +0400

    Add files via upload
    The basic exercises

---
+++ main.py
@@ -0,0 +1,71 @@
+# 1 ask for the user's first name
+
+name = input('enter your name here : \t')
+print('hello', ' my name is ', name)
+
+# 2 ask for the user's first name and surname
+
+firstname = input('please enter your first name:   ')
+surname = input('and your surname here :      ')
+print('Hello', firstname, surname)
+
+# 3 write code that will display the joke and the answer; using one line of code
+
+joke = input("What do you call a bear with no teeth? \n  - a gummy bear!")
+
+# 4 ask the user to enter two numbers. add them together and display the answer
+
+num1 = int(input("enter first number please :  "))
+num2 = int(input("enter second one:    "))
+print('the total answer is :  ', num1 + num2)
+
+# 5 ask user to enter three numbers. add together the first two and multiply this total by the third
+
+num1 = int(input('enter first number:    '))
+num2 = int(input('enter second number:   '))
+num3 = int(input('enter third number:    '))
+print('the answer is :', (num1 + num2) * num3)
+
+# 6 Ask the user for their name and their age. Add 1 to their age and display the output
+
+name = input(" What is your name?  \n ")
+age = int(input("enter your age :  \t"))
+print(name, " next birthday you'll be ", age + 1)
+
+# 7 Ask for the: total price of the bill; number of diners.
+# Divide the total bill by the number of diners and show how much each  person must pay.
+
+price = int(input(" total price of the bill :  "))
+number_of_diner = int(input("number of diners:  "))
+x = price / number_of_diner
+print("each person must pay: ", x)
+
+# 8 Write a program that will ask for a
+# number of days  and then will  show how many hours, minutes and seconds are in that number of days
+
+days = int(input("enter number of days :  "))
+hrs = days * 24
+minutes = hrs * 60
+seconds = minutes * 60
+print('in', days, "days there are: \n", hrs, 'hours \n', minutes, 'minutes \n', seconds, 'seconds')
+
+# 9 Ask the user to enter a weight in kilograms and convert it to pounds.
+weight = int(input("enter your weight in kg :  "))
+print('you are', weight * 2.204, 'in pounds')
+
+# 10 Task the user to enter a number over 100 and  a number under 10.
+# write how many times the smaller number goes into the larger one
+
+larger = int(input("enter a number over 100:  "))
+smaller = int(input("enter a number under 10:  "))
+answer = larger // smaller
+print(answer)
+
+
+
+
+
+
+
+
+
