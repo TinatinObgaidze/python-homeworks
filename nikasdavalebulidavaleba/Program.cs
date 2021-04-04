4b8cc43cb125fa1c665bb9c64ea188e6c423bde4
tinatin1obgaidze <74489333+tinatin1obgaidze@users.noreply.github.com>
2021-04-05 00:07:57 +0400

    Add files via upload

---
+++ nikasdavalebulidavaleba/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+// Tika Loves Nika
+namespace nikasdavalebulidavaleba
+
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            Console.WriteLine("Generating 10 random numbers:");
+            List<int> rndList = new List<int>();
+            for (int num = 1; num <= 10; num++)
+            {
+                int randnum = rand.Next(0, 100);
+                rndList.Add(randnum);
+                Console.WriteLine("number " +num + " of list =  "+ randnum);
+            }
+            List<int> listOfEvenNums = new List<int>();
+            List<int> listOfOddNums = new List<int>();
+            foreach (int ele in rndList)
+            {
+                if (ele % 2 == 0)
+                {
+                    listOfEvenNums.Add(ele);
+               
+                }
+                else
+                {
+                    listOfOddNums.Add(ele);
+                }
+            }   
+            foreach (int i in listOfEvenNums)
+            {
+                Console.WriteLine("Even numbers of list of even numbers are:    "+ i);
+
+            }
+
+            foreach (int i in listOfOddNums)
+            {
+                Console.WriteLine("Odd numbers of list of odd numbers are:      " + i);
+
+            }
+
+        }
+
+
+    }
+}
